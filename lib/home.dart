43a7dcbb8d881f7d0b1b8bcd9190bf77c7b16f74
Ashik03074 <ashik03074@gmail.com>
2021-06-04 10:55:05 +0600

    flutter navigation

---
+++ lib/home.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:listview_data/model/movie.dart';
+
+class MovieListView extends StatelessWidget {
+  final List<Movie> movieList = Movie.getMovies();
+  final List movies = [
+    "Titanic",
+    "Blade Runner",
+    "Rambo",
+    "The Avengers",
+    "Avatar",
+    "I Am Legend",
+    "300",
+    "The Wolf of the Wall Street",
+    "Interstllar",
+    "Game of Thrones",
+    "Vikings"
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Movies"),
+        //centerTitle: true,
+        backgroundColor: Colors.blueGrey.shade900,
+      ),
+      backgroundColor: Colors.blueGrey.shade400,
+      body: ListView.builder(
+          itemCount: movieList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return movieCard(movieList[index], context);
+            // return Card(
+            //   elevation: 4.5,
+            //   color: Colors.white,
+            //   child: ListTile(
+            //     leading: CircleAvatar(
+            //       child: Container(
+            //         width: 200,
+            //         height: 200,
+            //         decoration: BoxDecoration(
+            //           image: DecorationImage(
+            //             image: NetworkImage(movieList[index].images[0]),
+            //             fit: BoxFit.cover,
+            //           ),
+            //           //color: Colors.blue,
+            //           borderRadius: BorderRadius.circular(13.9),
+            //         ),
+            //         child: null,
+            //       ),
+            //     ),
+            //     trailing: Text("..."),
+            //     title: Text(movieList[index].title),
+            //     subtitle: Text("${movieList[0].title}"),
+            //     // onTap: () =>
+            //     //     debugPrint("Movie name : ${movies.elementAt(index)}"),
+            //     onTap: () {
+            //       Navigator.push(
+            //           context,
+            //           MaterialPageRoute(
+            //               builder: (context) => MovieListViewDetails(
+            //                     //movieName: movieList[index].title, /also works fime
+            //                     movieName: movieList.elementAt(index).title,
+            //                     movie: movieList[index],
+            //                   )));
+            //     },
+            //   ),
+            // );
+          }),
+    );
+  }
+}
+
+Widget movieCard(Movie movie, BuildContext context) {
+  return InkWell(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 120.0,
+        child: Card(
+          color: Colors.black45,
+          child: Padding(
+            padding: EdgeInsets.only(top: 8, bottom: 8, left: 54),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(movie.title),
+                    Text("Ratting : ${movie.imdbrating} /10"),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text("Released: ${movie.released}"),
+                    Text(movie.runtime),
+                    Text(movie.rated),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      onTap: () => {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => MovieListViewDetails(
+                          //movieName: movieList[index].title, /also works fime
+                          movieName: movie.title,
+                          movie: movie,
+                        )))
+          });
+}
+
+class MovieListViewDetails extends StatelessWidget {
+  final String movieName;
+  final Movie movie;
+
+  const MovieListViewDetails({Key key, this.movieName, this.movie})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          //title: Text("Movies $movieName"), also worksss fine
+          title: Text("Movies"),
+          backgroundColor: Colors.blueGrey.shade900,
+        ),
+        body: Container(
+          child: Center(
+            child: RaisedButton(
+              child: Text("${this.movie.director}"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:listview_data/home.dart';
+
+void main() => runApp(MaterialApp(
+      home: MovieListView(),
+    ));
